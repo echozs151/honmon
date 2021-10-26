e2dcfba9866919713547e9ca7d5fed65c20058a8
Konstantinos <kon.sv@outlook.com>
2021-10-26 19:56:40 +0300

    init

---
+++ src/main/java/com/example/honmon/HonmonApplication.java
@@ -0,0 +1,13 @@
+package com.example.honmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonmonApplication.class, args);
+	}
+
+}
