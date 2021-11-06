a45426be4efc5e442acfd5bc655d3e667656f20b
Konstantinos Sitnikov <echozs.151@gmail.com>
2021-11-06 14:28:05 +0200

    mongo dependency

--- src/main/java/com/example/honmon/Controllers/HomeController.java
+++ src/main/java/com/example/honmon/Controllers/HomeController.java
@@ -2,11 +2,9 @@ package com.example.honmon.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping()
--- src/main/java/com/example/honmon/HonmonApplication.java
+++ src/main/java/com/example/honmon/HonmonApplication.java
@@ -9,9 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
